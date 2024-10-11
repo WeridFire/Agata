e0bb8e6c74a9c241aeabbf74877a48944b559ecf
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2024-10-11 12:19:17 +0200

    updated pawns.h

---
+++ Agata/knights.cpp
@@ -0,0 +1,45 @@
+#include "knights.h"
+#include <cstdint>
+
+//        noNoWe    noNoEa
+//            +15  +17
+//             |     |
+//noWeWe  +6 __|     |__+10  noEaEa
+//              \   /
+//               >0<
+//           __ /   \ __
+//soWeWe -10   |     |   -6  soEaEa
+//             |     |
+//            -17  -15
+//        soSoWe    soSoEa
+
+uint64_t noNoEa(uint64_t b) {
+    return (b << 17) & notAFile;  // Movimento a nord-nord-est
+}
+uint64_t noEaEa(uint64_t b) {
+    return (b << 10) & notABFile; // Movimento a nord-est-est
+}
+uint64_t soEaEa(uint64_t b) {
+    return (b >> 6) & notABFile;  // Movimento a sud-est-est
+}
+uint64_t soSoEa(uint64_t b) {
+    return (b >> 15) & notAFile;  // Movimento a sud-sud-est
+}
+uint64_t noNoWe(uint64_t b) {
+    return (b << 15) & notHFile;  // Movimento a nord-nord-ovest
+}
+uint64_t noWeWe(uint64_t b) {
+    return (b << 6) & notGHFile;  // Movimento a nord-ovest-ovest
+}
+uint64_t soWeWe(uint64_t b) {
+    return (b >> 10) & notGHFile; // Movimento a sud-ovest-ovest
+}
+uint64_t soSoWe(uint64_t b) {
+    return (b >> 17) & notHFile;  // Movimento a sud-sud-ovest
+}
+
+uint64_t getKnightMoves(uint64_t knights) {
+    return noNoEa(knights) | noEaEa(knights) | soEaEa(knights) | soSoEa(knights) |
+        noNoWe(knights) | noWeWe(knights) | soWeWe(knights) | soSoWe(knights);
+}
+
